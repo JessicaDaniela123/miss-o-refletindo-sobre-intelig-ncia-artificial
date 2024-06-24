f26415e9d84aff3543a468804e21b3c9f24daa70
JessicaDaniela123 <102675820+JessicaDaniela123@users.noreply.github.com>
2024-06-24 20:49:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Com o intenso avanço das tecnologias, uma nova Inteligência Artificial é implantada no ambiente escolar, com o objetivo de tornar a aprendizagem mais dinâmica. Qual o primeiro pensamento?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: "No início ficou com medo das consequências desse novo avanço. "
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: "Quis saber mais sobre o funcionamento dessa IA."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Através de pesquisas acerca desta nova Inteligência Artificial, descobriu-se que em suas diversas funcionalidades, estão incluídas a capacidade de auxiliar na realização de trabalhos, bem como a capacidade de sanar as possíveis dúvidas dos estudantes. Qual o pensamento atribuído a essas funcionalidades?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Utilizar as funcionalidades desta IA pode trazer benefícios, visto que ela poderá ajudar a encontrar informações relevantes para futuros trabalhos e pesquisas, se utilizando de uma linguagem que facilite o entendimento.",
                 afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Ocorre um sentimento de receio pela possibilidade de se tornar dependente desta tecnologia.",
+                afirmacao: "Sente mais facilidade em utilizar seus próprios recursos para escrever e executar suas atividades."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Em uma das aulas ocorridas, o assunto foi colocado em pauta. O docente propõe que os estudantes utilizem essa nova tecnologia para desenvolver uma pesquisa. Como você realiza essa tarefa?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "UTiliza as informações fornecidas pela IA e não vê necessidade em fazer alterações nas informações obtidas, pois acredita em sua veracidade.",
+                afirmacao: "Obtém confiança nas informações que a IA proporciona."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Prefere utilizar as informções fornecidas pela IA apenas em poucas partes da pesquisa e vê a necessidade de buscar outras fontes de informação que podem transmitir maior confiança.",
+                afirmacao: "Prefere utilizar seus próprios métodos e dispensa a ajuda da IA."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Na data de entrega do trabalho, ocorreu um debate acerca dos impactos dessa IA. Como você se posiciona?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Defende que a IA pode ser vantajosa ao possibilitar a criação de novas oportunidades e melhorar as habilidades humanas.",
+                afirmacao: "Impulsiona a inovação na área da IA e defende a criação de novos caminhos profissionais que a envolvam."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Se mostra preocupado com a possibilidade de que esses novos avanços podem ocasionar a substituição dos seres humanos em diversas atividades.",
+                afirmacao: "Sua preocupação com as pessoas te motiva a discutir meios de utilização da IA de maneira ética."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Após um tempo da implementação da Inteligência Artificial no ambiente escolar surgiu outro trabalho, desta vez, em grupo, para ser realizado. Qual a melhor decisão a ser tomada? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Através das opiniões dos integrantes do grupo, acaba confiando na IA e se rende ao trabalho produzido por ela.",
+                afirmacao: "Percebe que confiar plenamente na IA, contribui para a diminuição da originalidade na execução de trabalhos."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Revisa as informações produzidas pela IA e adiciona contribuiuções e perspectivas do grupo.",
+                afirmacao: "Entende a importância de saber conciliar as funcionalidades da Inteligência Artificial com a criatividade e conhecimento humano. "
             }
         ]
     },
